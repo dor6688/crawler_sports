a182010bd1c26e4131110103f2aee69a0a22972b
dor6688 <dor6688@gmail.com>
2019-05-20 17:19:55 +0300

    threading

--- main.py
+++ main.py
@@ -2,27 +2,38 @@ import urllib.request
 from handle_databases import *
 import counter_word
 from tkinter import *
-
 import requests
 from bs4 import BeautifulSoup
+import threading
 
 
-# subject.get give us the user query
-# home_page = "https://www.sport5.co.il/"
+url = "https://www.sport5.co.il/"
+israeli_football = "https://www.sport5.co.il/world.aspx?FolderID=4439&lang=he"
+world_football = "https://www.sport5.co.il/world.aspx?FolderID=4453&lang=he"
+israeli_basketball = "https://www.sport5.co.il/world.aspx?FolderID=4467&lang=he"
+nba = "https://nba.sport5.co.il/NBA.aspx?FolderId=402&lang=HE"
+
+all_league_sport5 = {"israeli_football": israeli_football, "world_football": world_football,
+              "israeli_basketball": israeli_basketball, "nba": nba}
 
 
 def new_title(title):
+    """
+    :param title: get new title from web
+    :return: true if this title is exist in database, false otherwise
+    """
     return is_title_exist(title)
 
 
-
-def get_all_titles_from_sport5(url, web="sport5", cat="israeli_footbal"):
+def get_all_titles_from_sport5(url_page, web="sport5", cat="israeli_football"):
     """
-    :param url: get url page
+    :param url_page: get url page
+    :param web: which web
+    :param cat: sports category
     :return: all the title from current page
     """
     dic = {}
-    source = requests.get(url).text
+    source = requests.get(url_page).text
     soup = BeautifulSoup(source, features="html.parser")
     content = soup.find('div', class_="content-block")
     try:
@@ -67,9 +78,13 @@ def get_all_titles_from_sport5(url, web="sport5", cat="israeli_footbal"):
                 article_sport5(link, title, desc, web, cat)
 
 
-def article_sport5(url, title, desc, web, cat):
+def article_sport5(url_page, title, desc, web, cat):
     """
-    :param url: current page
+    :param url_page: current page
+    :param title: title of article
+    :param desc: description of article
+    :param web: web of the article
+    :param cat: category of the article
     :return: the word in the article
     """
     source = requests.get(url).text
@@ -77,9 +92,10 @@ def article_sport5(url, title, desc, web, cat):
     try:
         context = soup.find('div', id="find-article-content").text
         counter_word.start(context, title)
-        insert_new_article(web, cat, title, desc, url, context)
+        insert_new_article(web, cat, title, desc, url_page, context)
     except:
         print("Error !")
+
 
 # itay here
 def article_sport1(param):
@@ -100,6 +116,7 @@ def article_sport1(param):
             lines += line
         # counter_word.start(lines)
 
+
 # itay here
 def get_all_titles_from_sport1(url):
     """
@@ -117,7 +134,7 @@ def get_all_titles_from_sport1(url):
             clear_list.append(title)
     return clear_list
 
-
+'''
 def split_titles(titles):
     """
     :param titles: list with all url and title
@@ -126,29 +143,26 @@ def split_titles(titles):
     for title in titles:
         title_split = title.split("target=\"_self")
         if len(title_split) > 1:
-            article_sport5(str(title_split[0][1:len(title_split[0])-2]))
-        #else:
-            article_sport1(title[1:len(title)-1])
+            article_sport5(str(title_split[0][1:len(title_split[0]) - 2]))
+            # else:
+            article_sport1(title[1:len(title) - 1])
 
 
 def get_article_from_url(url):
     source = requests.get(url).text
     soup = BeautifulSoup(source, features="html.parser")
     print(soup.find('div', id="find-article-content").text)
+'''
 
 
-url = "https://www.sport5.co.il/"
-
-
-israeli_football = "https://www.sport5.co.il/world.aspx?FolderID=4439&lang=he"
-world_football = "https://www.sport5.co.il/world.aspx?FolderID=4453&lang=he"
-israeli_basketball = "https://www.sport5.co.il/world.aspx?FolderID=4467&lang=he"
-nba = "https://nba.sport5.co.il/NBA.aspx?FolderId=402&lang=HE"
-
-all_league = {"israeli_football":israeli_football, "world_football":world_football, "israeli_basketball":israeli_basketball, "nba":nba}
 
 
 def update_articles():
-    for league in all_league:
+    print("Start.. update article")
+    threading.Timer(60.0, update_articles).start()
+    for league in all_league_sport5:
         print(league)
-        titles = get_all_titles_from_sport5(all_league[league], "sport5", league)
+        get_all_titles_from_sport5(all_league_sport5[league], "sport5", league)
+    print("Finish.. update article")
+
+update_articles()
